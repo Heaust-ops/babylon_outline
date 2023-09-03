de22bd381abd6415f486be491a3159aa6e099b61
heaust <heaust.azure@gmail.com>
2023-09-03 11:20:04 +0530

    init

--- src/core/postprocess/postProcess.ts
+++ src/core/postprocess/postProcess.ts
@@ -5,7 +5,7 @@ import {
   Scene,
 } from "@babylonjs/core";
 
-export const applyPost = (camera: Camera, scene: Scene, engine: Engine) => {
+export const applyPost = (camera: Camera, _: Scene, engine: Engine) => {
   //
   new FxaaPostProcess("fxaa", 1.0, camera, undefined, engine, true);
 };
